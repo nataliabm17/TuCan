8692fa6c2a0c0ea96b3cb268a09e638cb5ccf39a
Patricia Quinde <patiquindec@gmail.com>
2019-06-09 17:54:10 -0600

    RTC test update

--- RTC_TimeLib.ino
+++ RTC_TimeLib.ino
@@ -2,80 +2,61 @@
 #include <TimeLib.h>
 #include <DS1307RTC.h>
 
-const char *monthName[12] = {
-"Jan", "Feb", "Mar", "Apr", "May", "Jun",
-"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-};
-
 tmElements_t tm;
 
+float mission_t = 0;
+
+void settingTime();
+void readingTime();
+
 void setup() {
-bool parse=false;
-bool config=false;
+  bool parse=false;
+  bool config=false;
 
-// get the date and time the compiler was run
-if (getDate(__DATE__) && getTime(__TIME__)) {
-parse = true;
-// and configure the RTC with this info
-if (RTC.write(tm)) {
-config = true;
-}
-}
-
-Serial.begin(9600);
-while (!Serial) ; // wait for Arduino Serial Monitor
-delay(200);
-if (parse && config) {
-Serial.print("DS1307 configured Time=");
-Serial.print(__TIME__);
-Serial.print(", Date=");
-Serial.println(__DATE__);
-} else if (parse) {
-Serial.println("DS1307 Communication Error :-{");
-Serial.println("Please check your circuitry");
-} else {
-Serial.print("Could not parse info from the compiler, Time=\"");
-Serial.print(__TIME__);
-Serial.print("\", Date=\"");
-Serial.print(__DATE__);
-Serial.println("\"");
-}
+  // get the time the compiler was run
+  //if (getTime(__TIME__)) {
+    //parse = true;
+    // and configure the RTC with this info
+    //if (RTC.write(tm)) {
+      //config = true;
+    //}
+  //}
+  settingTime();
+  Serial.begin(9600);
+  while (!Serial) ; // wait for Arduino Serial Monitor
+  delay(200);
 }
 
 void loop() {
+  //RTC.read(tm);
+  //Serial.print(tm.Hour);
+  //Serial.print(", ");
+  //Serial.print(tm.Minute);
+  //Serial.print(", ");
+  //Serial.print(tm.Second);
+  //Serial.print("\n");
+  readingTime();
+}
+
+bool getTime(const char *str){
+  int Hour, Min, Sec;
+
+  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
+  tm.Hour = Hour;
+  tm.Minute = Min;
+  tm.Second = Sec;
+  return true;
+}
+
+
+void settingTime(){
+  tm.Hour = 1;
+  tm.Minute = 0;
+  tm.Second = 0;
+}
+
+void readingTime(){
   RTC.read(tm);
-  Serial.print(tm.Hour);
-  Serial.print(", ");
-  Serial.print(tm.Minute);
-  Serial.print(", ");
-  Serial.print(tm.Second);
-  Serial.print("\n");
+  mission_t = (60*tm.Minute)+tm.Second;
+  
 }
-
-bool getTime(const char *str)
-{
-int Hour, Min, Sec;
-
-if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
-tm.Hour = Hour;
-tm.Minute = Min;
-tm.Second = Sec;
-return true;
-}
-
-bool getDate(const char *str)
-{
-char Month[12];
-int Day, Year;
-uint8_t monthIndex;
-
-if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false;
-for (monthIndex = 0; monthIndex < 12; monthIndex++) {
-if (strcmp(Month, monthName[monthIndex]) == 0) break;
-}
-if (monthIndex >= 12) return false;
-tm.Day = Day;
-tm.Month = monthIndex + 1;
-tm.Year = CalendarYrToTm(Year);
-return true;
-} 
