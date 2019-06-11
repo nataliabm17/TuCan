85f8df2ecc5d1cc1252cc3cd2ca042bb4a209f9b
Patricia Quinde <patiquindec@gmail.com>
2019-06-11 14:08:05 -0600

    manejo de color por voltaje

---
+++ rgbled.ino
@@ -0,0 +1,25 @@
+float yellow = ;
+float red = ;
+
+int redPin = 3;
+int bluePin = 4;
+int greenPin = 5;
+
+void setup(){
+    pinMode(redPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+}
+
+void rgbLED(){
+    if(voltage > yellow){                             //GREEN   
+        digitalWrite(greenPin, HIGH);
+    }else if((voltage < yellow) && (voltage > red)){  //YELLOW
+        digitalWrite(greenPin, HIGH);
+        digitlWrite(redPin,HIGH);
+    }else if(voltage < red){                          //RED
+        digitalWrite(greenPin,LOW);
+        delay(300);
+        digitalWrite(redPin,HIGH);
+    }
+}
